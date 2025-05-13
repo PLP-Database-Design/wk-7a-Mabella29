dbd31264097952e20784f0463b040674cc7a59c6
Mabella29 <nanaarquosuarh@gmail.com>
2025-05-13 15:00:58 +0000

    completed wk7a

---
+++ answers.sql
@@ -0,0 +1,63 @@
+-- Question 1: Achieving 1NF (First Normal Form)
+-- Creating ProductDetail table
+
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+
+-- Inserting data into ProductDetail table to achieve 1NF
+-- Each product in a separate row, even if it belongs to the same order and customer
+
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+    
+
+-- Question 2: Achieving 2NF (Second Normal Form)
+
+
+-- Creating Orders table
+
+CREATE TABLE Orders(
+    OrderID INT,
+    CustomerName VARCHAR(100),
+);
+
+-- Inserting data into Orders table to achieve 2NF
+-- Each order with its customer name, ensuring no partial dependency
+INSERT INTO Orders VALUES
+(101, 'John Doe'),
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(102, 'Jane Smith'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+-- Creating Product table
+
+CREATE TABLE Product (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
